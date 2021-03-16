f2c1663f772060597a679fa170242cb792aa74c2
Masao-Taketani <taket003@umn.edu>
2021-03-16 23:55:53 +0900

    need to implement AdaLIN

--- models/layers.py
+++ models/layers.py
@@ -41,8 +41,8 @@ class Downsample(tf.keras.layers.Layer):
 
 class ResnetBlock(tf.keras.layers.Layer):
 
-    def __init__(self, dim, use_bias):
-        super(ResnetBlock, self).__init__()
+    def __init__(self, dim, use_bias, name="ResnetBlock"):
+        super(ResnetBlock, self).__init__(name=name)
         self.conv2d_1 = tf.keras.layers.Conv2D(filters=dim, 
                                                kernel_size=3, 
                                                strides=1, 
@@ -71,11 +71,57 @@ class ResnetBlock(tf.keras.layers.Layer):
         return x
 
 
+class ResnetAdaLINBlock(tf.keras.layers.Layer):
+
+    def __init__(self, dim, use_bias, name="ResnetAdaLINBlock"):
+        super(ResnetAdaLINBlock, self).__init__(name=name)
+        self.conv2d_1 = tf.keras.layers.Conv2D(filters=dim, 
+                                               kernel_size=3, 
+                                               strides=1, 
+                                               use_bias=use_bias, 
+                                               kernel_initializer=KERNEL_INIT,
+                                               kernel_regularizer=KERNEL_REG)
+        self.instance_norm_1 = tfa.layers.InstanceNormalization()
+        self.relu = tf.keras.layers.ReLU()
+        self.conv2d_2 = tf.keras.layers.Conv2D(filters=dim, 
+                                               kernel_size=3, 
+                                               strides=1, 
+                                               use_bias=use_bias, 
+                                               kernel_initializer=KERNEL_INIT,
+                                               kernel_regularizer=KERNEL_REG)
+        self.instance_norm_2 = tfa.layers.InstanceNormalization()
+
+    def call(self, inputs):
+        x = reflection_pad_2d(inputs, 1)
+        x = self.conv2d_1(x)
+        x = self.instance_norm_1(x)
+        x = self.relu(x)
+        x = reflection_pad_2d(x, 1)
+        x = self.conv2d_2(x)
+        x = self.instance_norm_2(x)
+
+        return x        
+
+
+class AdaLIN(tf.keras.layers.Layer):
+
+    def __init__(self, dim, use_bias):
+        super(self, )
+
+
+class Upsample(tf.keras.layers.Layer):
+
+    def __init__(self,
+                 name='Upsample')
+        super(Upsample, self).__init__(name=name)
+
+
+
 class Generator(tf.keras.Layers.Layer):
 
     def __init__(self, first_filters=64, name="Generator"):
         super(Generator, self).__init__(name=name)
-        # Encoder Down-sampling part
+        # Used for Encoder Down-sampling part
         self.downsample_1 = Downsample(pad=3, 
                                        filters=first_filters, 
                                        kernel_size=7, 
@@ -93,7 +139,7 @@ class Generator(tf.keras.Layers.Layer):
                                        kernel_size=3,
                                        strides=2,
                                        use_bias=False)
-        # Encoder Bottleneck part
+        # Used for Encoder Bottleneck part
         self.resnet_block_1 = ResnetBlock(4 * first_filters,
                                           use_bias=False)
         self.resnet_block_2 = ResnetBlock(4 * first_filters,
@@ -102,4 +148,23 @@ class Generator(tf.keras.Layers.Layer):
                                           use_bias=False)
         self.resnet_block_4 = ResnetBlock(4 * first_filters,
                                           use_bias=False)
-        # CAM of Generator part
+        # Used for CAM of Generator part
+        self.gap_fc = tf.keras.layers.Dense(units=1, 
+                                            use_bias=False, 
+                                            kernel_initializer=KERNEL_INIT,
+                                            kernel_regularizer=KERNEL_REG)
+        self.gmp_fc = tf.keras.layers.Dense(units=1, 
+                                            use_bias=False, 
+                                            kernel_initializer=KERNEL_INIT,
+                                            kernel_regularizer=KERNEL_REG)
+        self.conv1x1 = tf.keras.layers.Conv2D(filters=4 * first_filters,
+                                              kernel_size=1,
+                                              strides=1,
+                                              use_bias=True,
+                                              kernel_initializer=KERNEL_INIT,
+                                              kernel_regularizer=KERNEL_REG)
+        self.relu = tf.keras.layers.ReLU()
+        # Used for Gamma, Beta part
+
+        # Uesd for Decoder Bottleneck part
+
